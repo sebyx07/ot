57b5dabe68f7ca1215ad984341f6ddee46105eb1
sebi <gore.sebyx@yahoo.com>
2018-01-10 12:27:08 +0200

    update

---
+++ insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO candidati (cnp, nume, prenume, adresa, media_bac, media_multianuala_mate)
+VALUES ('1940714350010', 'Alex', 'Popescu', 'Timisaora', '8.00', '8.20');
+
+INSERT INTO candidati (cnp, nume, prenume, adresa, media_bac, media_multianuala_mate)
+VALUES ('1940614350030', 'Andrei', 'Popescu', 'Arad', '7.00', '7.20');
